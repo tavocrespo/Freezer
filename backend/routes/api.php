c285f2048fc7a87bc679cac86555b01ab7271aaf
cristians-12 <cristian.sanchez.casd@gmail.com>
2024-11-26 15:27:06 -0500

    creacion de docker file sin configuracion

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,10 +1,20 @@
 <?php
 
+use App\Http\Controllers\ActivoController;
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 Route::controller(UserController::class)->group(function () {
     Route::get('/user/{id}', 'getUser');
     Route::post('/user', 'createUser');
-    Route::get('/users', 'getAllUsers');
 });
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::controller(ActivoController::class)->group(function () {
+        Route::get('/activo', 'getActivo');
+    });
+    Route::controller(UserController::class)->group(function () {
+        Route::get('/users', 'getAllUsers');
+    });
+});
+
